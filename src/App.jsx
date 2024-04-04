2b154df507d8737b3d806feea8db17251ac8e9de
unknown <abhinaykatta97@gmail.com>
2024-04-04 18:20:50 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" w-screen h-screen flex flex-col justify-center items-center">
+      <p>app</p>
+    </div>
+  );
+};
+
+export default App;
